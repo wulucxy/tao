c805356ee85e0fa33881f3441f51c0c66d92fc4d
qingtong <qingtong@wacai.com>
2016-02-02 09:21:52 +0800

    增加自动创建页面功能

---
+++ tasks/create.js
@@ -0,0 +1,42 @@
+var plugins = require('gulp-load-plugins')();
+var args = require('minimist')(process.argv.slice(2));
+var Promise = require('bluebird');
+var path = require("path");
+var fs = Promise.promisifyAll(require('fs-extra'));
+
+var dest = process.cwd();
+
+// -n 后面携带的项目工程名字
+if (!args.n) {
+	console.log('请使用 -n 参数设置项目名称， 比如 -n test');
+	process.exit(0);
+}
+var projectName = args.n.toString();
+var examplePath = path.resolve(dest, 'src', 'example');
+
+function generateStructure(project){
+  return fs.copyAsync(examplePath, project,{clobber: true})
+    .then(function(err){
+      if (err) return console.error(err);
+    });
+}
+
+module.exports = function () {
+	return function () {
+		
+		if (projectName) {
+			dest = path.resolve(dest, 'src', 'pages',projectName);
+		}
+
+		if (fs.existsSync(dest)){
+			var files = fs.readdirSync(dest);
+			if (files.length > 0) {
+				console.error(dest + ' 目录不为空, 项目创建失败');
+				return;
+			}
+		}
+
+		generateStructure(dest);
+
+	};
+};
