c172e115bdb8a2c56bd229fde378cae1447b10e3
qingtong <qingtong@wacai.com>
2016-02-22 10:22:30 +0800

    资讯

--- src/pages/bookService/index.js
+++ src/pages/bookService/index.js
@@ -12,22 +12,77 @@ var common = require("../../assets/components/common");
 
 //自定义功能写下面
 var tmpl = require("./templates/apply.ejs");
-$("#applyBtn").on("click",function(e){
-	var btn = $(e.target);
-	modalBox(btn,{
-		html:tmpl(),
-		klass : 'w540 shadow',
-        closeByOverlay : false,
-        completeCallback : function(){
-        	//checkbox定制
-			$('.label_radio').click(function(){
-			  util.setupLabel();
+require("../../assets/components/validator");
+
+
+var book = {
+
+	init : function(){
+		var that = this;
+		$("#applyBtn").on("click",function(e){
+			var btn = $(e.target);
+			modalBox(btn,{
+				html:tmpl(),
+				klass : 'w540 shadow',
+		        closeByOverlay : false,
+		        startCallback : function(){
+		        	//checkbox定制
+					$('.label_radio').click(function(){
+					  util.setupLabel();
+					});
+
+					util.setupLabel();
+		        },
+		        completeCallback : function(){
+ 					that.formAction(btn);
+		        }
 			});
 
-			util.setupLabel();
+		});
+	},
 
-			
-        }
-	});
+	formAction : function(){
+		var that = this;
 
-});
+		$("#bookForm").validator({
+			errorParent : ".row",
+			successCallback : function(e){
+				 var target = $(e.target).closest('.btn');
+				 that.postBookInfo(target,$("#bookForm"));
+			}
+		});
+	},
+
+	postBookInfo : function(){
+		var that = this;
+		var province = $("[name=province]").val();
+		$.ajax({
+			url : province+"/tzy/appointment/create",
+			type : "post",
+			data : $("bookForm").serialize(),
+			success : function(res){
+				if(typeof res == "string"){
+					var res = $.parseJSON(res);
+				}
+
+				if(res.code != 200){
+					warn(res.msg);
+					return;
+				}
+
+				
+
+			},
+			error : function(){
+				warn("网络错误，请稍后再试");
+			}
+		})
+	}
+
+};
+
+
+book.init();
+
+
+
